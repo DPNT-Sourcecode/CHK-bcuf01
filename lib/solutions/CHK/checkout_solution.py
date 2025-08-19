dd79deb01e092ff037088d47746fbfc3bf819e93
sbx_user1051 <sbx_user1051@169.254.69.207>
2025-08-19 15:53:37 +0000

    Tue Aug 19 15:53:37 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,4 +3,13 @@ class CheckoutSolution:
 
     # skus = unicode string
     def checkout(self, skus):
-        raise NotImplementedError()
+        a=skus.count('A')             
+        b=skus.count('B')
+        c=skus.count('C')
+        d=skus.count('B')
+        a3=int(a/3)
+        b2=int(b/2)
+        a=a-a3
+        b=b-b2
+        return 50*a+30*b+20*c+15*d+130*a3+45*b2
+    
