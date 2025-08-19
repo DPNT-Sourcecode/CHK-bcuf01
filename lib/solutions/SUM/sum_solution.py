038f4275a08d4fce7a846428e09a1b29e4838287
sbx_user1051 <sbx_user1051@169.254.69.207>
2025-08-19 15:17:54 +0000

    Tue Aug 19 15:17:54 UTC 2025

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -2,9 +2,17 @@
 class SumSolution:
     
     def compute(self, x, y):
-        if (x is int) and (y is int):
-            (if x>=0) and (y>=0):
-                if (x<101) and (y<101):
+        if self.check_type(x,y):
+            if self.check_positive(x,y):
+                if self.check_upper_limit(x,y):
                     return x+y
-        
+    
+    def check_type(self, x,y):
+        return isinstance(x, int) and isinstance(y,int)    
+    
+    def check_positive(self, x, y):
+        return (x>=0) and (y>=0)
+    
+    def check_upper_limit(self, x, y):
+        return (x<101) and (y<101)
 
--- test/solution_tests/SUM/test_sum_solution.py
+++ test/solution_tests/SUM/test_sum_solution.py
@@ -4,3 +4,17 @@ from solutions.SUM.sum_solution import SumSolution
 class TestSum():
     def test_sum(self):
         assert SumSolution().compute(1, 2) == 3
+
+    def test_integer():
+        assert SumSolution().check_type(1,2)
+        assert SumSolution().check_type('two', 1.2) == False
+
+    def test_positive():
+        assert SumSolution().check_positive(1,2)
+        assert SumSolution().check_positive(-1,2) == False 
+        assert SumSolution().check_positive(-1,-2) == False
+
+    def test_upper_limit():
+        assert SumSolution().check_upper_limit(100,2)
+        assert SumSolution().check_upper_limit(101,200)
+       
