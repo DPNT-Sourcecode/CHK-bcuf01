ae5ffd49582bffd5ac02890e943b12b9d8020539
sbx_user1051 <sbx_user1051@169.254.3.185>
2025-08-21 13:17:59 +0000

    Thu Aug 21 13:17:59 UTC 2025

--- test/solution_tests/CHK/test_checkout_solution.py
+++ test/solution_tests/CHK/test_checkout_solution.py
@@ -1,20 +1,12 @@
-from solutions.SUM.sum_solution import SumSolution
+from solutions.CHK.checkout_solution import CheckoutSolution
 
 
-class TestSum():
-    def test_sum(self):
-        assert SumSolution().compute(1, 2) == 3
+class CheckoutSolution():
+    def test_checkoutA(self):
+        assert CheckoutSolution().checkout('A') == 50
 
-    def test_integer():
-        assert SumSolution().check_type(1,2)
-        assert SumSolution().check_type('two', 1.2) == False
+    def test_checkoutMultiple(self):
+        assert CheckoutSolution().checkout('BB') == 45
+        assert CheckoutSolution().checkout('AAA') == 130
 
-    def test_positive():
-        assert SumSolution().check_positive(1,2)
-        assert SumSolution().check_positive(-1,2) == False 
-        assert SumSolution().check_positive(-1,-2) == False
-
-    def test_upper_limit():
-        assert SumSolution().check_upper_limit(100,2)
-        assert SumSolution().check_upper_limit(101,200)
        
