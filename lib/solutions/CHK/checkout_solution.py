72f35884448ed58e19d5163b0b293f11972d30ac
sbx_user1051 <sbx_user1051@169.254.3.185>
2025-08-21 13:08:59 +0000

    Thu Aug 21 13:08:59 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -4,18 +4,19 @@ class CheckoutSolution:
     # skus = unicode string
     def checkout(self, skus):
         result=0
+        illegal_input=0
         prices={'A':50, 'B':30, 'C':20,'D':15}
         for item in prices.keys():
             x=skus.count(item)
-            
-        result+=x*prices[item]
-        a=skus.count('A')             
-        b=skus.count('B')
-        c=skus.count('C')
-        d=skus.count('D')
-        a3=int(a/3)
-        b2=int(b/2)
-        a=a-a3
-        b=b-b2
-        return 50*a+30*b+20*c+15*d+130*a3+45*b2
-    
+            illegal_input+=x
+            if item == 'A':
+                result+=x%3*prices[item]+int(x/3)*130
+            elif item == 'B':
+                result+=x%2*prices[item]+int(x/2)*45
+            else:
+                result+=x*prices[item]
+
+        return result
+
+
+print(CheckoutSolution.checkout(1,'CD'))
