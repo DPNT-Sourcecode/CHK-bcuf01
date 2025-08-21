745b0205e26239e83742a7617b64c4bb45013e44
sbx_user1051 <sbx_user1051@169.254.3.185>
2025-08-21 12:59:59 +0000

    Thu Aug 21 12:59:59 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,10 +3,16 @@ class CheckoutSolution:
 
     # skus = unicode string
     def checkout(self, skus):
+        result=0
+        prices={'A':50, 'B':30, 'C':20,'D':15}
+        for item in prices.keys():
+            x=skus.count(item)
+            
+        result+=x*prices[item]
         a=skus.count('A')             
         b=skus.count('B')
         c=skus.count('C')
-        d=skus.count('B')
+        d=skus.count('D')
         a3=int(a/3)
         b2=int(b/2)
         a=a-a3
