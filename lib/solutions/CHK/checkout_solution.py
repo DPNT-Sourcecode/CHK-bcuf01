a41306add8ed86fdcaeb173e0e5294dc0374b78f
sbx_user1051 <sbx_user1051@169.254.90.117>
2025-08-23 19:15:58 +0000

    Sat Aug 23 19:15:58 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,14 +1,10 @@
 import numpy as np
-def buy_x_get_y_free(num,x):
-    if int(num/x)>0:
-        discount=int(num/x)
-    return(discount)
+
 def read_items_from_file(f,o):
     for line in f:
         if line.startswith("|"):
             if line.__contains__('Item'):
                 continue     
-            #print('17',line[17])
             if line[17]!=' ':
                 offers=line[17:]
                 offers=offers[:-2].strip()
@@ -41,9 +37,7 @@ class CheckoutSolution:
     
     def count_chars(self, string):
         u, counts = np.unique(list(string), return_counts=True)
-        #print(u, counts)
         self.counts=dict(zip(u,counts))
-        #print(self.counts)
 
     def new_item(self,key,price):
         self.price[key]=price
@@ -78,7 +72,7 @@ class CheckoutSolution:
         self.counts[key_item]-=n_items*discount
         self.price[offer_key]=new_value
         self.counts[offer_key]=discount
-        #print(discount)
+
 
     def get_offer(self,f_type, offer_key):
         input=self.offers[f_type][offer_key]
@@ -90,9 +84,7 @@ class CheckoutSolution:
 
     def checkout(self, skus):
         result=0
-        #illegal_input=0
         self.count_chars(skus)
-        #prices={'A':50, 'B':30, 'C':20,'D':15, 'E':40, 'F':10}
         
         #execute all offers of type zero first
         for offer in self.offers[0].keys():
@@ -110,66 +102,6 @@ class CheckoutSolution:
         for k in self.counts.keys():
             if k not in self.price.keys():
                return -1   
-            #print(self.counts[k]*self.price[k] ) 
-            #print('key',k, self.price[k])    
-            #if         
-            #print(self.counts[k])
-            #print(type(self.price[k]))
             result+=self.counts[k]*self.price[k] 
         return result           
-'''
-
-        for item in prices.keys():
-            x=skus.count(item)
-            print('x', x)
-            print('item', item)
-            illegal_input+=x
-            if item == 'A':
-                if int(x/5)>0:
-                    remainder=x%5
-                    result+=remainder%3*prices[item]+int(remainder/3)*130+int(x/5)*200  
-                else:    
-                    result+=x%3*prices[item]+int(x/3)*130
-            elif item == 'B':
-                discount=int(skus.count('E')/2)
-                if x>0:
-                    x-=discount
-                print('x', x)
-                result+=x%2*prices[item]+int(x/2)*45
-            else:
-                if item == 'F':    
-                    x-=int(x/3)
-                result+=x*prices[item]
-            print('result', result)    
-        if len(skus)-illegal_input>0:
-            result=-1'''
-     
-
-
-#f=open("challenges/CHK_R4.txt")
-c=CheckoutSolution()
-
-
-        
-print(c.get_price('X'))        
-print(buy_x_get_y_free(5,3))
-print(c.price)
-#print(c.offers)
-#print(c.count_chars('ABBBC'))
-c.count_chars('ABBBC')
-counts=c.counts
-print(c.counts['A'] == 1)
-print(c.counts['B'] == 3)
-print(c.counts['C'] == 1)
-print(c.checkout('BB') == 45)
-print(c.checkout('AAA') == 130)
-print(c.checkout('AAAAA') == 200)
-print(c.checkout('EEB') == 80)
-print(c.checkout('EE') == 80)
-print(c.checkout('FFF') == 20)
-print(c.checkout('FFFFFF') == 40)
-print(c.checkout('}'))
-
-
-
 
